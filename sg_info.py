0ba8a65f0dd1bb1ced0a16ba2576a5f653490dfa
battlesysadmin <battlesysadmin@users.noreply.github.com>
2016-04-21 11:07:55 -0700

    from qwiklabs class
    full, with hashes - need AWS credentials

---
+++ sg_info.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import boto.ec2
+import hashlib
+
+try:
+     conn = boto.ec2.connect_to_region ("us-west-2")
+     current_sgs = conn.get_all_security_groups()
+except boto.exception.BotoServerError, e:
+     log.error(e.error_message)
+conn.close()
+
+for sg in current_sgs:
+     print "="*72
+     print "id:\t\t", sg.id
+     print "name:\t\t", sg.name
+     print "vpc:\t\t", sg.vpc_id
+     print "instance:\t", sg.instances()
+     print " "
+     print "ingress rules:"
+     for rule in sg.rules:
+         ruledata = sg.id,rule.grants,rule.ip_protocol,rule.from_port,rule.to_port,"ingress"
+         rulehash=hashlib.sha256(str(ruledata)).hexdigest()
+         print "\thash:",rulehash
+         print "\t",rule.grants,"-> [instance]:",rule.from_port,"-",rule.to_port,rule.ip_protocol
+         print "egress rules:"
+     for rule in sg.rules_egress:
+         ruletuple = sg.id,rule.grants,rule.ip_protocol,rule.from_port,rule.to_port,"ingress"
+         rulehash=hashlib.sha256(str(ruletuple)).hexdigest()
+         print "\thash:",rulehash
+         print "\t[instance]->",rule.grants,":",rule.from_port,"-",rule.to_port,rule.ip_protocol
+         print " "
+         print "="*72
